c950005dcb5a2584588e6a38306db8d07954a45b
Nathan King <nathan.riley.k@gmail.com>
2026-04-05 11:52:02 -0400

    added backtracking

---
+++ LCS.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+class LongestCommonSubsequence:
+    def __init__(self, str1, str2, weights):
+        self.A = str1
+        self.B = str2
+        self.n = len(str1)
+        self.m = len(str2)
+        self.arr = np.zeros((self.n+1, self.m+1), dtype=int)
+        self.weights_dict = weights
+    
+
+    def longestCommonSubsequence(self):
+        s = ""
+        for i in range(self.n, -1, -1):
+            for j in range(self.m, -1, -1):
+                if i==self.n or j == self.m:
+                    self.arr[i,j] = 0
+
+                elif self.A[i] != self.B[j]:
+                    self.arr[i,j] = max(self.arr[i, j+1], self.arr[i+1,j]) #there not equal so we cant take the letter
+                else:
+                    self.arr[i,j] = max(self.arr[i, j+1], self.arr[i+1,j], self.weights_dict[self.A[i]] + self.arr[i+1,j+1]) #the last value is when we choose this letter to be a part of the final subsequence
+        i = 0
+        j = 0
+        while i < self.n and j < self.m:
+            if self.A[i] == self.B[j]:
+                if self.arr[i,j] == self.arr[i+1, j+1] + self.weights_dict[self.A[i]]: # this means we chose this to be part of the final subsequence
+                    s += self.A[i]
+                    i +=1
+                    j += 1
+                elif self.A[i,j] == self.A[i+1, j]: #can i go down 1 row and get same optimal value?
+                    i +=1
+                else:
+                    j +=1
+                #must do cases where they arent equal
+            else:
+                if self.A[i,j] == self.A[i+1, j]:
+                    i += 1
+                else:
+                    j += 1
+        return (self.arr[0,0], s)
+    
+    
+                 
+        
+
+
+
+        
+
+
+        
